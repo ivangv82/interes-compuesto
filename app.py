47691c8c1a5fcaeeef24d9d788915ecef8273dfc
ivangv82 <ivangvregistros@yahoo.es>
2025-04-01 18:08:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-st.title("Calculadora de Interés Compuesto - Visualización Mejorada")
+st.title("Calculadora de Interés Compuesto")
 
 # --------------------
 # Entradas en la barra lateral
